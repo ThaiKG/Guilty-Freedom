e98bd5f63cfe04367368cd0106a1128058a44665
iamnicuo <98766406+iamnicuo@users.noreply.github.com>
2023-02-02 08:01:19 -0600

    random code

--- Guilty Freedom/Assets/Scripts/wallRunning.cs
+++ Guilty Freedom/Assets/Scripts/wallRunning.cs
@@ -9,10 +9,13 @@ public class wallRunning : MonoBehaviour
     public LayerMask whatIsGround;
     public float wallRunForce;
     public float wallClimbSpeed;
+    public float wallJumpUpForce;
+    public float wallJumpSideForce;
     public float maxWallRunTime;
     private float wallRunTimer;
 
     [Header("Input")]
+    public KeyCode jumpKey = KeyCode.Space;
     public KeyCode upwardsRunKey = KeyCode.LeftShift;
     public KeyCode downwardsRunKey = KeyCode.LeftControl;
     private bool upwardsRunning;
@@ -76,6 +79,11 @@ public class wallRunning : MonoBehaviour
         {
             if (!pm.wallrunning)
                 StartWallRun();
+
+            if (Input.GetKeyDown(jumpKey))
+            {
+                WallJump();
+            }
         }
 
         // State 3 - None
@@ -84,6 +92,7 @@ public class wallRunning : MonoBehaviour
             if (pm.wallrunning)
                 StopWallRun();
         }
+
     }
 
     private void StartWallRun()
@@ -121,4 +130,20 @@ public class wallRunning : MonoBehaviour
     {
         pm.wallrunning = false;
     }
+
+    private void WallJump()
+    {
+        // enter exiting wall state
+        //exitingWall = true;
+        //exitWallTimer = exitWallTime;
+
+        Vector3 wallNormal = wallRight ? rightWallhit.normal : leftWallhit.normal;
+
+        Vector3 forceToApply = transform.up * wallJumpUpForce + wallNormal * wallJumpSideForce;
+
+        // reset y velocity and add force
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(forceToApply, ForceMode.Impulse);
+    }
+
 }
