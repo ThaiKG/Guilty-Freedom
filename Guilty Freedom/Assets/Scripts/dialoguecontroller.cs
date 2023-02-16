4499e5063b314a868f540ed8701762f2667fa47a
Sam Qiao <sam.qiao07@gmail.com>
2023-02-16 08:05:18 -0600

    e

--- Guilty Freedom/Assets/Scripts/dialoguecontroller.cs
+++ Guilty Freedom/Assets/Scripts/dialoguecontroller.cs
@@ -18,6 +18,7 @@ public class dialoguecontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (d1) {
             thing.idx = 0;
             thing.textSpeed = 0.1f;
@@ -33,14 +34,14 @@ public class dialoguecontroller : MonoBehaviour
         } else if (d2) {
             thing.idx = 0;
             thing.textSpeed = 0.1f;
-            string[] temp = {"Psssssst...", "We can help you escape the plantation and gain your freedom", "First complete the platform level and get all of the coin bags in the neighboring village", "Touch the tree on the platform level to teleport to the village", "Good luck!"};
+            string[] temp = {"Psssssst...", "We can help you escape the plantation and gain your freedom", "First complete the platform level and get all of the coin bags in the neighboring village", "Go into the Magic Tree or the portal to start your journey", "The magic tree is hidden, but it will grant your a reward", "Good luck!"};
             thing.lines = temp;
             // thing.StartDialogue();
             dialogue.SetActive(true);
             d2 = false;
             
             if (thing.done) {
-                SceneManager.LoadScene(1);
+                // SceneManager.LoadScene(1);
                 thing.done = false;
             }
         }
