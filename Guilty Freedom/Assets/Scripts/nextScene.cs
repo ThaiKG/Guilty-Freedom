ecd7fd61ad08a35b6ab8d3a83c8e498e25ec0ccc
iamnicuo <98766406+iamnicuo@users.noreply.github.com>
2023-02-23 07:46:00 -0600

    small changes

--- Guilty Freedom/Assets/Scripts/nextScene.cs
+++ Guilty Freedom/Assets/Scripts/nextScene.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class nextScene : MonoBehaviour
 {
+
+    public int[] luck;
+
     // Start is called before the first frame update
     void Start()
     {
