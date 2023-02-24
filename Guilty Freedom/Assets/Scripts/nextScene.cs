fe5c7e0ca062c0e44182de0a8202d69f5a729618
iamnicuo <98766406+iamnicuo@users.noreply.github.com>
2023-02-24 07:16:21 -0600

    idk

--- Guilty Freedom/Assets/Scripts/nextScene.cs
+++ Guilty Freedom/Assets/Scripts/nextScene.cs
@@ -11,15 +11,20 @@ public class nextScene : MonoBehaviour
     private string treeidx;
 
     // Start is called before the first frame update
-    void Start()
+    void randidx()
     {
         rand = Random.Range(0, 3);
         treeidx = tree[rand].name;
+    }
+
+    private void Update()
+    {
         Debug.Log(rand);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        randidx();
         if (collision.gameObject.CompareTag(treeidx))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
