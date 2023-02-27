c0247fff495ecef4d5a85a00da32da7bbbd77279
Thai Truong <harry.truong102@gmail.com>
2023-02-27 08:03:36 -0600

    dfdfsdfs

--- Guilty Freedom/Assets/Scripts/NPCInteractable.cs
+++ Guilty Freedom/Assets/Scripts/NPCInteractable.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour
 {
+    public GameObject dialogue;
+    private Dialogue thing;
+
+    void start(){
+        thing = dialogue.GetComponent<Dialogue>();
+    }
     public void Interact(){
         Debug.Log("Interact!");
     }
