d4f55affbda721f7496bdee68e2f0ff4a63dcac9
iamnicuo <98766406+iamnicuo@users.noreply.github.com>
2023-02-28 08:41:46 -0600

    AI

---
+++ Guilty Freedom/Assets/Scripts/NPC/asProjectile.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asProjectile : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 3f);
+        Vector3 temp = new Vector3(0, 180, 0);
+        transform.LookAt(EnemyAI.player, temp);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
