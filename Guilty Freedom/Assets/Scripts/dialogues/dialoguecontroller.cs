4610a190854a80b021759012e0e71d08cf7880db
Thai Truong <harry.truong102@gmail.com>
2023-03-01 07:56:45 -0600

    vcxvxcv

--- Guilty Freedom/Assets/Scripts/dialogues/dialoguecontroller.cs
+++ Guilty Freedom/Assets/Scripts/dialogues/dialoguecontroller.cs
@@ -18,7 +18,7 @@ public class dialoguecontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
         
     }
     void OnTriggerEnter(Collider other) {
