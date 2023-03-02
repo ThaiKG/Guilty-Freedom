70ff18c8893decc36068395c122b685eb11d7ed4
Thai Truong <harry.truong102@gmail.com>
2023-03-02 07:39:05 -0600

    sds

--- Guilty Freedom/Assets/Scripts/NPC/EnemyAI.cs
+++ Guilty Freedom/Assets/Scripts/NPC/EnemyAI.cs
@@ -87,7 +87,7 @@ public class EnemyAI : MonoBehaviour
         {
             ///Attack code here
             Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.forward * 20f, ForceMode.Impulse);
             rb.AddForce(transform.up * 8f, ForceMode.Impulse);
             ///End of attack code
 
