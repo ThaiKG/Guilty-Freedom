ef4df807802f9fc7eaf7fa6057bdfac7c7718c24
Sam Qiao <sam.qiao07@gmail.com>
2023-03-07 08:02:35 -0600

    niashfbafbkjfnbkjfbkjlfnsafaifbawjifnskfskjfskjfskjfnskjfnskjfnsdjknsjkfskjfskjfnskjfgsijrfrfskjfSDJfnbkfbsjkfskjfnbsd
    niashfbafbkjfnbkjfbkjlfnsafaifbawjifnskfskjfskjfskjfnskjfnskjfnsdjknsjkfskjfskjfnskjfgsijrfrfskjfSDJfnbkfbsjkfskjfnbsd

--- Guilty Freedom/Assets/Scripts/player/PlayerHealth.cs
+++ Guilty Freedom/Assets/Scripts/player/PlayerHealth.cs
@@ -4,30 +4,39 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-
     public int maxHealth = 100;
     public int currentHealth;
-
     public HealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
-        healthBar = GetComponent<HealthBar>();
+        // healthBar = GetComponent<HealthBar>();
         healthBar.SetMaxHealth(maxHealth);
+        Debug.Log(healthBar);
     }
 
     public void TakeDamage(int damage){
         currentHealth -= damage;
+        Debug.Log(healthBar);
         healthBar.SetHealth(currentHealth);
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        // if (Input.GetKey(KeyCode.Space))
+        // {
+        //     TakeDamage(5);
+        // }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("axe"))
         {
             TakeDamage(5);
         }
+
     }
 
 }
