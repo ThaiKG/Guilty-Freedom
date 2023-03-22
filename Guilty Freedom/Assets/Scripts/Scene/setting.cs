f378f719fc49c9ffc3ad6eabe686f11475e71575
Sam Qiao <sam.qiao07@gmail.com>
2023-03-22 07:44:58 -0500

    e

--- Guilty Freedom/Assets/Scripts/Scene/setting.cs
+++ Guilty Freedom/Assets/Scripts/Scene/setting.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using UnityEngine.Audio;
+[RequireComponent(typeof(Slider))]
 public class setting : MonoBehaviour
 {
+    Slider slider {
+        get {return GetComponent<Slider>();}
+    }
+
+    public AudioMixer mixer;
+    public string volumeName;
+    public void UpdateValueOnChange(float change) {
+        mixer.SetFloat(volumeName, change);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
